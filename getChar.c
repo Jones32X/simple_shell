cee7a69b7a8a4aa96b91c063647b0e42601ff94e
Jones32X <jonespedro.Nezingu@gmail.com>
2024-04-11 05:45:10 -0700

    +1 chgs

--- getChar.c
+++ getChar.c
@@ -1,25 +1,27 @@
 #include "simple.h"
 
 /**
- * _getchar - get each character of the line command
- * Return: command line or EOF.
+ * _getchar - get each char of the command line
+ * Return: cmd line or EOF.
  **/
+
 int _getchar(void)
 {
 	static unsigned char buff[BUFF_SIZE];
+	static int cnt;
 	static int index;
-	static int count;
 
 	index = 0;
-	count = 0;
-	if (index >= count)
+	cnt = 0;
+
+	if (index >= cnt)
 	{
 		index = 0;
-		count = read(STDIN_FILENO, buff, BUFF_SIZE);
-		printf("%i\n", count);
-		if (count == 0)
+		cnt = read(STDIN_FILENO, buff, BUFF_SIZE);
+		printf("%i\n", cnt);
+		if (cnt == 0)
 			return (EOF);
-		if (count < 0)
+		if (cnt < 0)
 			return (EOF);
 	}
 	return (buff[index++]);
